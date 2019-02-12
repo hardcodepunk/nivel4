2fa5a656ee392d562b763d31c30858d5713b9702
friederjanmoerman <hijbrid@gmail.com>
2019-02-12 12:19:21 -0300

    finished the arrow animation and functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 document.getElementById('js-toggle-mobile-nav').onclick = function() {
   document.getElementById('body').classList.toggle('open-menu');
 }
+
+document.getElementById('js-start-scrolling').onclick = function() {
+  document.getElementById('services').scrollIntoView({behavior: "smooth"});
+}
