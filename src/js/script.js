5369a63760d022d31da335febb7362bb0988f413
friederjanmoerman <hijbrid@gmail.com>
2019-03-21 23:59:28 +0100

    finalized spanish version

--- src/js/script.js
+++ src/js/script.js
@@ -8,13 +8,18 @@ document.getElementById('js-toggle-mobile-nav').onclick = function() {
 
 // close mobile menu when clicking on a nav item
 for (var i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener('click', function() {
-    console.log('clicked');
+  navItem[i].addEventListener('click', function(event) {
 
     if (body.classList.contains('open-menu')) {
-      console.log('has-open-menu');
       body.classList.remove('open-menu');
     }
+
+    //get href of nav element and scroll to its section
+    var idDestination = this.getAttribute("href").substring(1, this.length);
+    var destination = document.getElementById(idDestination);
+    destination.scrollIntoView({behavior: "smooth"});
+
+    event.preventDefault();
   })
 };
 
