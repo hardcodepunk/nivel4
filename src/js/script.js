f4ba454301b3c2d21683095f7143f32bb1299b77
friederjanmoerman <hijbrid@gmail.com>
2019-04-10 01:09:35 +0200

    fixed z-index header conflicting with leaflet map z-index

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ var body = document.getElementById('body');
 var mymap = L.map('map').setView([-33.429055, -70.621262], 17);
 var marker = L.marker([-33.429055, -70.621262]).addTo(mymap);
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNqdWFjbHhpMDAxOGkzeW1pcnBnY2pwajAifQ.iBRgDkBbAEAGieVZURxAzA', {
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNqdWFjdWY3MDAxYmIzeWw0bjlzaW80cmUifQ.3HAer8K3D-7fu29uIPPfVA', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
