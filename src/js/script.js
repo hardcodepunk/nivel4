a0a8c6484cb4d9edc5d0f881ae1602bc1a71c662
friederjanmoerman <hijbrid@gmail.com>
2019-04-16 00:46:31 +0200

    made ready for production

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,13 @@ var body = document.getElementById('body');
 var mymap = L.map('map').setView([-33.429055, -70.621262], 17);
 var marker = L.marker([-33.429055, -70.621262]).addTo(mymap);
 
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNqdWFjdWY3MDAxYmIzeWw0bjlzaW80cmUifQ.3HAer8K3D-7fu29uIPPfVA', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaGFyZGNvZGVwdW5rIiwiYSI6ImNqdWFjdWY3MDAxYmIzeWw0bjlzaW80cmUifQ.3HAer8K3D-7fu29uIPPfVA', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox.streets'
+}).addTo(mymap);
 
 // open mobile menu when clicking on mobile nav icon
 document.getElementById('js-toggle-mobile-nav').onclick = function() {
